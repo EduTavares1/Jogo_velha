416ae31a19419c37e649b3da725ca76c00288930
EduTavares1 <eduardotavares790@gmail.com>
2024-12-15 16:02:11 -0300

    ADD : Regra_3

--- código sem IA/jogador_ia.py
+++ código sem IA/jogador_ia.py
@@ -7,10 +7,22 @@ from tabuleiro import Tabuleiro
 class JogadorIA(Jogador):
     def __init__(self, tabuleiro : Tabuleiro, tipo : int):
         super().__init__(tabuleiro, tipo)
+
+    def R_3(self) -> (int,int):
+        lista = []
+        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
+            self.matriz[1][1] == Tabuleiro.JOGADOR_0
+            return 1,1
+        return None
             
 
     def getJogada(self) -> (int, int):
         lista = []
+
+        meio = self.R_3()
+        if meio:
+            return meio
+        
         for l in range(0,3):
             for c in range(0,3):
                 if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
