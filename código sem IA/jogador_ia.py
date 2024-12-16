7188466cdaedd7311699568acca8955cdeb61741
EduTavares1 <eduardotavares790@gmail.com>
2024-12-16 19:32:44 -0300

    att: consertando a logica e implementando regra 6 e 5

--- código sem IA/jogador_ia.py
+++ código sem IA/jogador_ia.py
@@ -8,28 +8,38 @@ class JogadorIA(Jogador):
     def __init__(self, tabuleiro : Tabuleiro, tipo : int):
         super().__init__(tabuleiro, tipo)
 
-    def R_3(self) -> (int,int):
-        lista = []
-        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
-            self.matriz[1][1] == Tabuleiro.JOGADOR_0
-            return 1,1
-        return None
+    
+               
+                      
+               
             
 
     def getJogada(self) -> (int, int):
-        lista = []
 
-        meio = self.R_3()
-        if meio:
-            return meio
+        #Regra 4
         
+        
+        #Regra 5
+        jogada = self.Regra_5()
+        if jogada:
+            return jogada
+
+        #Regra 6
+        return self.Regra_6()
+    
+    def Regra_5(self) -> (int, int):
+        
+        extremidades =[(0,0),(0,2),(2,0),(2,2)]
+
+        for extremo in extremidades:
+            l,c = extremo
+            if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                return(l,c)
+        return None    
+
+    def Regra_6(self) -> (int,int):
         for l in range(0,3):
             for c in range(0,3):
                 if self.matriz[l][c] == Tabuleiro.DESCONHECIDO:
-                    lista.append((l, c))
-                    
-        if(len(lista) > 0):
-            p = randint(0, len(lista)-1)
-            return lista[p]
-        else:
-           return None
+                    return (l, c)
+        return None            
