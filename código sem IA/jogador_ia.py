66829a70aa9c74abfda32468893abd7437b31377
EduTavares1 <eduardotavares790@gmail.com>
2024-12-17 10:26:58 -0300

    add: regra 4

--- código sem IA/jogador_ia.py
+++ código sem IA/jogador_ia.py
@@ -7,6 +7,7 @@ from tabuleiro import Tabuleiro
 class JogadorIA(Jogador):
     def __init__(self, tabuleiro : Tabuleiro, tipo : int):
         super().__init__(tabuleiro, tipo)
+        self.tipo_d_oponente = Tabuleiro.JOGADOR_X if tipo == Tabuleiro.JOGADOR_0 else Tabuleiro.JOGADOR_0
         
 
     
@@ -19,9 +20,15 @@ class JogadorIA(Jogador):
     def getJogada(self) -> tuple[int, int] | None:
 
         #Regra 3
-        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
-            return (1,1)
+        jogada = self.Regra_3()
+        if jogada:
+            return jogada
 
+        #Regra 4
+        jogada = self.Regra_4()
+        if jogada:
+            return jogada
+        
         
         #Regra 5
         jogada = self.Regra_5()
@@ -30,6 +37,21 @@ class JogadorIA(Jogador):
 
         #Regra 6
         return self.Regra_6()
+    
+    def Regra_3(self) -> tuple[int, int] | None:
+        if self.matriz[1][1] == Tabuleiro.DESCONHECIDO:
+            return (1,1)
+
+    def Regra_4(self) -> tuple[int, int] |  None:
+        extremidades = [(0,0), (0,2),(2,0),(2,2)]
+
+        for extremo in extremidades:
+            l,c = extremo
+            canto_oposto = (2-l, 2-c)
+            if self.matriz[l][c] == self.tipo_d_oponente and self.matriz[canto_oposto[0]][canto_oposto[1]] == Tabuleiro.DESCONHECIDO:
+                return canto_oposto
+        return None
+
     
     
     
